53976b045f7f54e044faeac8f2dd8a32f5fda604
IT24100637 <it24100637@my.sliit.lk>
2026-05-03 14:21:42 +0530

    update some mobile files

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -1,6 +1,7 @@
 import { Platform } from 'react-native';
 
 const envBaseUrl = process.env.EXPO_PUBLIC_API_BASE_URL;
+export const API_BASE_URL = 'https://moto-hub-smart-vehicle-service-yqfh.onrender.com/api';
 
 const devHost = Platform.select({
   android: '10.0.2.2',
